137cfda2a71bf3efc30e971377311086a800a53f
Vlad <v.dutchak@365.softlist.ua>
2022-10-10 16:23:30 +0300

    Update 2

--- task_2/solution.py
+++ task_2/solution.py
@@ -4,6 +4,7 @@ from sqlite3 import Cursor
 import pyodbc,os,click
 from datetime import date,datetime
 
+
 class Sql:
     def __init__(self, database, server="DESKTOP-SU7SNM7\SQLEXPRESS01"):
         self.cnxn = pyodbc.connect("Driver={SQL Server Native Client 11.0};"
@@ -12,14 +13,21 @@ class Sql:
                                    "Trusted_Connection=yes;")
         self.query = "-- {}\n\n-- Made in Python".format(datetime.now()
                                                          .strftime("%d/%m/%Y"))
+
+
 class TODO(object):
+
     __sql = Sql('todoDB')
     __cursor = __sql.cnxn.cursor()
     __ToDoTable = 'ToDoTable'
     __DoneTable = 'DoneItems'
     __separator = ';'
+
     def addItem(self):
+
         item = input(f'Введіть задачу, яку потрібно виконати\n(Якщо ви хочете добавити декілька використовуйте {self.__separator} як знак розділення)\n')
+        os.system('cls')
+
         if self.__separator not in item:
             self.__cursor.execute(f"INSERT INTO {self.__ToDoTable} (Item) VALUES ('{item}')")
             self.__cursor.commit()
@@ -27,13 +35,18 @@ class TODO(object):
             items = item.split(self.__separator)
             for i in items:
                 self.__cursor.execute(f"INSERT INTO {self.__ToDoTable} (Item) VALUES ('{i.strip()}')")
+
             self.__cursor.commit()
+
         print("Задачу успішно додано!")
+
     def showItems(self):
         self.__cursor.execute(f'SELECT * FROM {self.__ToDoTable} where IsDone = 0')
         row = self.__cursor.fetchone()
+
         if row is None:
             print("На даний момент, список задач пустий!")
+            
         while row:
             status = ""
             if row[2]:
@@ -42,27 +55,34 @@ class TODO(object):
                 status = "Не виконано"
             print(str(row[0]) + '. ' + row[1] + ' -- ' + status )
             row = self.__cursor.fetchone()
+
     def performItem(self):
         self.showItems()
         id = input("Введіть ід задачі\n>>>>")
         os.system('cls')
+
         self.__cursor.execute(f'update {self.__ToDoTable} set IsDone = 1 where ItemId = {id}')
         self.__cursor.execute(f"insert into {self.__DoneTable} (ItemId,[Date]) values ({str(id)},'{str(date.today()).replace('-','.')}')")
         self.__cursor.commit()
+        
         print("Дані успішно редаговано!")
+
     def showDoneItems(self):
         self.__cursor.execute(f'select {self.__DoneTable}.ItemId,Item,[Date] from {self.__DoneTable} join {self.__ToDoTable} on {self.__DoneTable}.ItemId = {self.__ToDoTable}.ItemId order by [Date]')
         row = self.__cursor.fetchone()
         counter = 1 
+
         while row:
             print(str(counter) + '. ' + str(row[2]) + ' ' + row[1] )
             row = self.__cursor.fetchone()
             counter +=1
+    
     def deleteItem(self):
         while(True):
             self.showItems()
             id = input("Введіть id задачі,яку хочете видалити:\n>>>>")
             os.system('cls')
+
             try:
                 self.__cursor.execute(f'DELETE FROM {self.__ToDoTable} where ItemId = {id}')
                 self.__cursor.commit()
@@ -73,10 +93,12 @@ class TODO(object):
             finally:
                 print("Задачу успішно видалено!")
                 break
+    
     def deleteDoneItem(self):
         self.showDoneItems()
         date = input('Введіть дату по якій хочете видалити записи, або ж * якщо хочете видалити все.\n>>>>')
         os.system('cls')
+
         if date is '*':
             self.__cursor.execute(f'TRUNCATE TABLE {self.__DoneTable}')
             self.__cursor.execute(f'DELETE FROM {self.__ToDoTable} where IsDone = 1')
@@ -88,15 +110,19 @@ class TODO(object):
             self.__cursor.execute(f"SELECT [ItemId] FROM {self.__DoneTable} where [Date] = '{date}'")
             row = self.__cursor.fetchone()
             listId = []
+
             if row is None:
                 print('Помилка введення дати')
             else:
                 while row:
                     listId.append(row[0])
                     row = self.__cursor.fetchone()
+            
             self.__cursor.execute(f"DELETE FROM {self.__DoneTable} where [Date] = '{date}'")
+
             for id in listId:
                 self.__cursor.execute(f"DELETE FROM {self.__ToDoTable} where ItemId = {id}")
+
             self.__cursor.commit()
             print(f'Виконані справи за {date} успішно видалено')
             
@@ -107,6 +133,7 @@ class TODO(object):
 
 
 if __name__ == '__main__':
+    todo = TODO()
     os.system('cls')
     menu = """1. Переглянути список невиконаних задач
 2. Додати одну або декілька задач
@@ -115,7 +142,7 @@ if __name__ == '__main__':
 5. Переглянути список виконаних задач
 6. Очистити список виконаних задач за конкретний день
 0. Зупинити програму"""
-    todo = TODO()
+
     while True:
         print('Меню')
         print(menu)
